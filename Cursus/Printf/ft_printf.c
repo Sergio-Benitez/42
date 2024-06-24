46c61aad7a65394a314a215bc5823cad32b57365
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-06-24 22:23:54 +0200

    .

--- Cursus/Printf/ft_printf.c
+++ Cursus/Printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:10:56 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/06/24 22:19:17 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/06/24 22:23:44 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,8 @@ int	ft_printf(char const *format, ...)
 				printint(va_arg(ap, int), &count);
 			}
 //			else if(format[i+1] == 'u')
-//			else if(format[i+1] == 'x')
-//			else if(format[i+1] == 'X')
+//			else if(format[i+1] == 'x' || format[i+1] == 'X')
 ;	}
-	///
 	va_end(ap);
 	return (count);
 }
