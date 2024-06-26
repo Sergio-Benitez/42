7e507f22b89ca774b03f633cb7cb663d9c53e514
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-06-26 21:27:20 +0200

    all working (line too long)

--- Cursus/ft_printf_OG.c
+++ Cursus/ft_printf_OG.c
@@ -6,32 +6,11 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 20:10:56 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/06/26 21:26:49 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/06/26 21:05:01 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static void	check_param(char f, va_list *ap, int *i, int *count)
-{
-	if (f == '%')
-		printchar('%', count);
-	else if (f == 'c')
-		printchar(va_arg(*ap, int), count);
-	else if (f == 's')
-		printstr(va_arg(*ap, char *), count);
-	else if (f == 'p')
-	{
-		*count += write(1, "0x", 2);
-		printptr(va_arg(*ap, void *), count);
-	}
-	else if (f == 'd' || f == 'i')
-		printint(va_arg(*ap, int), count);
-	else if (f == 'u')
-		printuns(va_arg(*ap, unsigned int), count);
-	else if (f == 'x' || f == 'X')
-		printhex(va_arg(*ap, unsigned int), f, count);
-}
 
 int	ft_printf(char const *format, ...)
 {
@@ -44,14 +23,47 @@ int	ft_printf(char const *format, ...)
 	va_start(ap, format);
 	while (format[i])
 	{
-		if (format[i] == '%')
-		{
-			i++;
-			check_param(format[i], &ap, &i, &count);
-			i++;
-		}
-		else
+		if (format[i] != '%')
 			printchar(format[i++], &count);
+		else
+		{
+			if (format[i + 1] == '%')
+			{
+				i += 2;
+				printchar('%', &count);
+			}
+			else if (format[i + 1] == 'c')
+			{
+				i += 2;
+				printchar(va_arg(ap, int), &count);
+			}
+			else if (format[i + 1] == 's')
+			{
+				i += 2;
+				printstr(va_arg(ap, char *), &count);
+			}
+			else if (format[i + 1] == 'p')
+			{
+				i += 2;
+				count += write(1, "0x", 2);
+				printptr(va_arg(ap, void *), &count);
+			}
+			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				i += 2;
+				printint(va_arg(ap, int), &count);
+			}
+			else if (format[i + 1] == 'u')
+			{
+				i += 2;
+				printuns(va_arg(ap, unsigned int), &count);
+			}
+			else if (format[i + 1] == 'x' || format[i + 1] == 'X')
+			{
+				i += 2;
+				printhex(va_arg(ap, unsigned int), format[i - 1], &count);
+			}
+		}
 	}
 	va_end(ap);
 	return (count);
