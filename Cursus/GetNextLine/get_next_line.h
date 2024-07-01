f78db2e52bba9603e5593674ef52c36ea6ca5ea9
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-07-01 20:00:52 +0200

    getting horny :$

---
+++ Cursus/GetNextLine/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 19:44:27 by sbenitez          #+#    #+#             */
+/*   Updated: 2024/07/01 19:59:30 by sbenitez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
