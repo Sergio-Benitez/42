0e8a370d1e1922e8459708c4cc4ed992508fb9cf
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-07-02 01:13:54 +0200

    ?

--- Cursus/GetNextLine/get_next_line.c
+++ Cursus/GetNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 19:44:34 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/01 19:45:42 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/07/01 20:17:12 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 char	*get_next_line(int fd)
 {
-	
+	ssize_t	bytes_read;
+
+//	bytes_read = read(fd, );
 }
--- Cursus/GetNextLine/get_next_line.h
+++ Cursus/GetNextLine/get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 19:44:27 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/01 19:59:30 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/07/01 20:29:28 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE	42
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
