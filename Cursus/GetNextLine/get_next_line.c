4d2d2dc4b2d7d0be3ef826cceb6a59b80eb16880
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-07-03 20:01:55 +0200

    no aidia

--- Cursus/GetNextLine/get_next_line.c
+++ Cursus/GetNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 19:44:34 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/01 20:17:12 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/07/03 12:39:23 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,29 @@ char	*get_next_line(int fd)
 	ssize_t	bytes_read;
 
 //	bytes_read = read(fd, );
+}
+
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+	
+	if (argc != 2)
+		return (1);
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
