2b4fba30784af28a13cf6877e255ad27f38ca874
sergio-benitez <sergio.benitez.ramirez@gmail.com>
2024-07-08 21:53:06 +0200

    a la tercera noseké

--- Cursus/GetNextLine_Weno/get_next_line.h
+++ Cursus/GetNextLine_Weno/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 19:44:27 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/05 11:43:40 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/07/08 12:59:30 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void	*ft_realloc(void *ptr, size_t new_size);
 size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_free(char *buffer);
 
 #endif
