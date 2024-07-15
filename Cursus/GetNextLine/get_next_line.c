92a87ff63c48c73fce3c4498eaaa653e0cd75a2f
sergio-benitez <sergio.benitez.ramirez@gmail.com>
2024-07-15 22:30:55 +0200

    almost fixed

--- Cursus/GetNextLine/get_next_line.c
+++ Cursus/GetNextLine/get_next_line.c
@@ -6,19 +6,93 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 21:26:36 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/08 21:52:40 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/07/15 22:29:49 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "get_next_line.h"
+
+char	*ft_joinfree(char *buffer, char *aux)
+{
+	char	*temp;
+
+	temp = ft_strjoin(buffer, aux);
+	free(buffer);
+	return (temp);
+}
 
 char	*ft_readbuffer(int fd, char *buffer)
 {
 	int		i;
 	char	*aux;
-	if (!buffer)
-		return (NULL);
 	
+	if (!buffer)
+	{
+		buffer = ft_calloc(1, 1);
+		if (!buffer)
+			return (NULL);
+	}
+	aux = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	i = 1;
+	while ((!ft_strchr(aux, '\n')) && i > 0)
+	{
+		i = read(fd, aux, BUFFER_SIZE);
+		if (i == -1)
+			return (free(aux), NULL);
+		aux[i] = '\0';
+		buffer = ft_joinfree(buffer, aux);
+		if (!buffer)
+			return (NULL);
+	}
+	free(aux);
+	return (buffer);
+}
+
+char	*ft_readline(char *buffer)
+{
+	char	*line;
+	int		i;
+	
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_calloc(i + 1 + (buffer[i] == '\n'), sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i] = '\n';
+	return (line);
+}
+
+char	*ft_updatebuffer(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*upt;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+		return (ft_free(buffer));
+	upt = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	if (!upt)
+		return (ft_free(buffer));
+	i++;
+	j = 0;
+	while (buffer[i])
+		upt[j++] = buffer[i++];
+	free(buffer);
+	return (upt);	
 }
 
 char	*get_next_line(int fd)
@@ -30,14 +104,13 @@ char	*get_next_line(int fd)
 		return (NULL);
 	buffer = ft_readbuffer(fd, buffer);
 	if (!buffer)
-		return(free(buffer), buffer = NULL, NULL);
-	
-
-	return(line);
+		return (ft_free(buffer));
+	line = ft_readline(buffer);
+	buffer = ft_updatebuffer(buffer);
+	return (line);
 }
 
 #include <fcntl.h>
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -55,11 +128,12 @@ int	main(int argc, char **argv)
 		perror("Error al abrir el archivo");
 		return (1);
 	}
-	while (!(line = get_next_line(fd)) != NULL)
+	while ((line = get_next_line(fd)) != NULL)
 	{
 		printf("%s", line);
 		free(line);
 	}
+
 	close(fd);
 	return (0);
 }
--- Cursus/GetNextLine/get_next_line.h
+++ Cursus/GetNextLine/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 21:26:49 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/08 21:28:18 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/07/15 21:26:08 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void	*ft_realloc(void *ptr, size_t new_size);
 size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_free(char *buffer);
 
 #endif
