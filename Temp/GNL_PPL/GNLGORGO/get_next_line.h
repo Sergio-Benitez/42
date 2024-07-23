3406046de4d4860d571d08297da758edf086a6cd
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-07-23 11:40:58 +0200

    mira yokese

---
+++ Temp/GNL_PPL/GNLGORGO/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jormoral <jormoral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 17:49:12 by jormoral          #+#    #+#             */
+/*   Updated: 2024/06/10 19:40:20 by jormoral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+int		ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, const char *s2);
+char	*ft_free(char *buffer);
+
+#endif
