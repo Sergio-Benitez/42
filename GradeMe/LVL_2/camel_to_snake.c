f5a9183b32c6dc837f7a5acbe9b9bfc6c1e2690c
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-08-15 21:13:14 +0200

    noce

---
+++ GradeMe/LVL_2/camel_to_snake.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   camel_to_snake.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 20:28:01 by sbenitez          #+#    #+#             */
+/*   Updated: 2024/08/15 21:07:32 by sbenitez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	camel_to_snake(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] > 64 && str[i] < 91)
+		{
+			str[i] += 32;
+			write(1, "_", 1);
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	camel_to_snake(argv[1]);
+	return (0);
+}
