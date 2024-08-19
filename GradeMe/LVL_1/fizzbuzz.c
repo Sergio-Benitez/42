7cff357b0d23d99651e5fe8890dee7be3843d7f8
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-08-19 14:08:45 +0200

    ñiñiñiñiñiñiñiñiñ

--- GradeMe/LVL_1/fizzbuzz.c
+++ GradeMe/LVL_1/fizzbuzz.c
@@ -5,45 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/25 13:11:18 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/07/25 13:25:16 by sbenitez         ###   ########.fr       */
+/*   Created: 2024/08/19 12:53:03 by sbenitez          #+#    #+#             */
+/*   Updated: 2024/08/19 13:18:30 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	fizzbuzz(void)
+void putnbr(int i)
 {
-	int	i;
-
-	i = 1;
-	while (i < 101)
-	{
-		if (i % 3 == 0)
-		{
-			if (i % 5 == 0)
-				write(1, "fizzbuzz\n", 9);
-			else
-				write(1, "fizz\n", 5);
-		}
-		else if (i % 5 == 0)
-		{
-			if (i % 3 == 0)
-				write(1, "fizzbuzz\n", 9);
-			else
-				write(1, "buzz\n", 5);
-		}
-		else
-		{
-			write(1, &i, 1);
-			write(1, "\n", 1);
-		}
-		i++;
-	}
+	char	str[10] = "0123456789";
+	
+	if (i > 9)
+		putnbr(i / 10);
+	write(1, &str[i % 10], 1);
 }
 
 int	main(void)
 {
-	fizzbuzz();
+	int	i;
+
+	i = 1;
+
+	while (i < 101)
+	{
+		if (i % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			putnbr(i);
+		write(1, "\n", 1);
+		i++;
+	}
 	return (0);
 }
--- GradeMe/LVL_2/ft_atoi.c
+++ GradeMe/LVL_2/ft_atoi.c
@@ -1,18 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   atoi.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/15 21:10:32 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/08/15 21:11:14 by sbenitez         ###   ########.fr       */
+/*   Created: 2024/08/19 13:20:26 by sbenitez          #+#    #+#             */
+/*   Updated: 2024/08/19 13:45:35 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-int	*ft_range(int start, int end)
+int	ft_atoi(const char *str)
 {
+	int	result;
+	int	sign;
 	
+	result = 0;
+	sign = 1;
+
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
 }
