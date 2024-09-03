ac11facca8e817f08f080f0a9c6f3e0f3c265bc9
Sergio-Benitez <sergio.benitez.ramirez@gmail.com>
2024-09-03 14:27:22 +0200

    nocekepone

---
+++ GradeMe/LVL_2/print_bits.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_bits.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 12:25:17 by sbenitez          #+#    #+#             */
+/*   Updated: 2024/09/03 13:16:20 by sbenitez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	int				i = 8;
+	unsigned char	bit;
+
+	while (i--)
+	{
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
+	}
+}
