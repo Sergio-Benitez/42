54fcd3ad51759a73850308f752fecccee5191f9b
sergio-benitez <sergio.benitez.ramirez@gmail.com>
2024-11-25 23:13:39 +0100

    casi finito, falla make bonus y no guarda kpasa

--- So_Long_BONUS/inc/so_long.h
+++ So_Long_BONUS/inc/so_long.h
@@ -6,15 +6,13 @@
 /*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 17:12:29 by sbenitez          #+#    #+#             */
-/*   Updated: 2024/11/11 13:23:39 by sbenitez         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:51:58 by sbenitez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # define TILE_SIZE 96
-# define NUM_WALL_TEXTURES 3
-# define NUM_COLLECT_TEXTURES 2
 
 # include "../Libft/inc/libft.h"
 # include "../Libft/inc/ft_printf.h"
@@ -44,9 +42,6 @@ typedef struct s_map
 	int			exit_n;
 	t_coords	exit_pos;
 	int			collect_n;
-	t_coords	*collect_pos;
-	int			enemy_n;
-	t_coords	enemy_pos;
 }				t_map;
 
 typedef struct s_game
@@ -54,21 +49,32 @@ typedef struct s_game
 	mlx_t		*mlx;
 	t_map		*map;
 	mlx_image_t	*img_bg;
-	mlx_image_t	*img_wall[NUM_WALL_TEXTURES];
-	mlx_image_t *img_player;
+	mlx_image_t	*img_wall;
+	mlx_image_t	*img_player;
 	mlx_image_t	*img_player_up;
-	mlx_image_t *img_player_down;
-	mlx_image_t *img_player_left;
-	mlx_image_t *img_player_right;
+	mlx_image_t	*img_player_down;
+	mlx_image_t	*img_player_left;
+	mlx_image_t	*img_player_right;
 	mlx_image_t	*img_collect;
-	mlx_image_t	*collect_frame[NUM_COLLECT_TEXTURES];
-	int			current_frame;
-	mlx_image_t	*img_exit_open;
-	mlx_image_t	*img_exit_closed;
+	mlx_image_t	*img_exit;
 	int			move_count;
 	mlx_image_t	*wmoves;
 	int			frame_count;
 }				t_game;
+
+void		free_matrix(void **matrix, int height);
+void		ft_free_exit(char **map_data, t_map *map);
+void		free_textures(t_game *game);
+void		cleanup(t_game *game);
+int			check_map(t_map *map);
+
+void		ft_hook(void *param);
+void		handle_input(t_game *game);
+void		handle_movement(t_game *game, t_coords old_pos, t_coords new_pos);
+void		update_flags(t_map *map);
+
+void		init_game_struct(t_game *game);
+void		init_game(t_game *game, t_map *map);
 
 int			check_type(char *name);
 int			check_chars(t_map *map);
@@ -79,28 +85,11 @@ int			check_path(t_map *map);
 void		find_pos(t_map *map);
 t_map		*get_map(char **map_data, char *file);
 t_map		*load_create_map(char *file);
-void		ft_free_exit(char **map_data, t_map *map);
+char		**copy_map_data(char **data, int height, int width);
 void		flood_fill(t_map *map, char **map_data, int y, int x);
 
-char		**copy_map_data(char **data, int height, int width);
-void		free_matrix(void **matrix, int height);
-void		update_flags(t_map *map);
-mlx_image_t	*select_wall_texture(int x, int y, t_game *game);
-int			check_map(t_map *map);
-
-void		ft_hook(void *param);
 int			charge_textures(t_game *game);
-void		handle_input(t_game *game);
-void		handle_movement(t_game *game, t_coords old_pos, t_coords new_pos);
-
 void		render_map(t_game *game);
 void		render_tile(t_game *game, int i, int j);
-void		cleanup(t_game *game);
-void		free_textures(t_game *game);
-
-void		init_game_struct(t_game *game);
-void		init_game(t_game *game, t_map *map);
-
-void		ft_moves(t_game *game);
 
 #endif
---
+++ So_Long_BONUS/src/mandatory/game_hooks.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_hooks.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 10:53:16 by sbenitez          #+#    #+#             */
+/*   Updated: 2024/11/25 21:54:46 by sbenitez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/so_long.h"
+
+void	ft_hook(void *param)
+{
+	t_game	*game;
+
+	game = (t_game *)param;
+	if (game->frame_count < 0)
+	{
+		game->frame_count++;
+		if (game->frame_count == 0)
+			mlx_close_window(game->mlx);
+		return ;
+	}
+	if (game->frame_count++ < 5)
+		return ;
+	game->frame_count = 0;
+	handle_input(game);
+	render_map(game);
+}
+
+void	handle_input(t_game *game)
+{
+	t_coords	old_pos;
+	t_coords	new_pos;
+
+	old_pos = game->map->player_pos;
+	new_pos = old_pos;
+	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(game->mlx);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_UP)
+		|| mlx_is_key_down(game->mlx, MLX_KEY_W))
+		new_pos.y -= 1;
+	else if (mlx_is_key_down(game->mlx, MLX_KEY_DOWN)
+		|| mlx_is_key_down(game->mlx, MLX_KEY_S))
+		new_pos.y += 1;
+	else if (mlx_is_key_down(game->mlx, MLX_KEY_LEFT)
+		|| mlx_is_key_down(game->mlx, MLX_KEY_A))
+		new_pos.x -= 1;
+	else if (mlx_is_key_down(game->mlx, MLX_KEY_RIGHT)
+		|| mlx_is_key_down(game->mlx, MLX_KEY_D))
+		new_pos.x += 1;
+	handle_movement(game, old_pos, new_pos);
+}
+
+void	handle_movement(t_game *game, t_coords old_pos, t_coords new_pos)
+{
+	if (game->map->data[new_pos.y][new_pos.x] != '1' && !(
+			game->map->data[new_pos.y][new_pos.x] == 'E'
+			&& game->map->collect_n > 0))
+	{
+		if (new_pos.x != old_pos.x || new_pos.y != old_pos.y)
+		{
+			if (game->map->data[new_pos.y][new_pos.x] == 'C')
+			{
+				game->map->data[new_pos.y][new_pos.x] = '0';
+				game->map->collect_n--;
+				if (game->map->collect_n == 0)
+					game->map->update_flags[game->map->exit_pos.y]
+					[game->map->exit_pos.x] = true;
+			}
+			game->map->player_pos = new_pos;
+			game->move_count++;
+			ft_printf("Number of movements: %d\n", game->move_count);
+			if (game->map->data[new_pos.y][new_pos.x] == 'E'
+				&& game->map->collect_n == 0)
+				game->frame_count = -20;
+			game->map->update_flags[old_pos.y][old_pos.x] = true;
+			game->map->update_flags[new_pos.y][new_pos.x] = true;
+		}
+	}
+}
+
+void	update_flags(t_map *map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	map->update_flags = malloc(map->height * sizeof(bool *));
+	while (i < map->height)
+	{
+		map->update_flags[i] = malloc(map->width * sizeof(bool));
+		j = 0;
+		while (j < map->width)
+		{
+			map->update_flags[i][j] = true;
+			j++;
+		}
+		i++;
+	}
+}
