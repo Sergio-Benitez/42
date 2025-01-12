849511fae25e68e0cc87d42fff8360742ec71be1
sergio-benitez <sergio.benitez.ramirez@gmail.com>
2025-01-12 20:40:12 +0100

    push_swap empesamo

---
+++ Push_Swap/src/parseo.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parseo.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 12:40:46 by sbenitez          #+#    #+#             */
+/*   Updated: 2025/01/12 12:52:17 by sbenitez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2) // no params = return control
+		return (0);
+	ft_check_numbers(argv); // Error\n = check_enteros, check_><entero, check_duplicados
+}
